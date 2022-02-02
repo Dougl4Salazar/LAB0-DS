e7558b332dc938348b4e225df7c0c19edd500a26
Dougl4Salazar <92768320+Dougl4Salazar@users.noreply.github.com>
2022-02-02 17:31:31 -0600

    Helpers

---
+++ LAB0DS/Helpers/Data.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB0DS.Helpers
+{
+    public class Data
+    {
+        private static Data _instance;
+        public Data Instance
+        {
+            get { return _instance; }
+            set { _instance = value; }
+        }
+
+    }
+}
