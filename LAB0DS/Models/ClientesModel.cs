0edcadab7d920f9bd74ddc3deb35eaf70db8c4d0
Dougl4Salazar <92768320+Dougl4Salazar@users.noreply.github.com>
2022-02-02 16:52:04 -0600

    Propiedades

---
+++ LAB0DS/Models/ClientesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB0DS.Models
+{
+    public class ClientesModel
+    {
+        public int Telefono{get; set; }
+        public string M { get; set; }
+    }
+}
